02b8c68a1404ea08321d344836197605dd0364cf
chiupatil1511 <70797015+chiupatil1511@users.noreply.github.com>
2021-08-15 15:04:23 +0530

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2)x!lq_yaioz9@dn$v__xtaw5e)5vys94l_caz_pjvnh=0f6$='
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,8 +126,45 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shivanipatil1527@gmail.com'
-EMAIL_HOST_PASSWORD = 'shivanishreyans1527'
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = 'xyz'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#2)x!lq_yaioz9@dn$v__xtaw5e)5vys94l_caz_pjvnh=0f6$=
